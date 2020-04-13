5ba9daf29eaf098cdb6a9fa0527842ca1103761b
ptriggins <ptriggins@crimson.ua.edu>
2020-04-13 01:23:20 -0400

    Updates for example classes

--- Globe Example Code/Tile.cs
+++ Globe Example Code/Tile.cs
@@ -1,9 +1,26 @@
-public class Tile {
-  
-    public float HeightValue { get; set; }
-    public int X, Y;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
 
-    public Tile()
-    {
-    }
+public enum HeightType
+{
+	DeepWater = 1,
+	ShallowWater = 2,
+	Shore = 3,
+	Sand = 4,
+	Grass = 5,
+	Forest = 6,
+	Rock = 7,
+	Snow = 8
+}
+
+public class Tile
+{
+	public HeightType HeightType;
+	public float HeightValue { get; set; }
+	public int X, Y;
+
+	public Tile()
+	{
+	}
 }
