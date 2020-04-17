bb7689a314986e58a19dc3e0db8b392d5a242a26
ptriggins <ptriggins@crimson.ua.edu>
2020-04-17 11:52:33 -0400

    Simplified Tile Class

---
+++ myCityGen/Assets/Scripts/PopulationMap/Tile.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum DensityType
+{
+    // urban
+    // suburban
+    // rural
+}
+
+public class Tile
+{
+    public DensityType DensityType;
+
+    public float DensityValue { get; set; }
+    public int X, Y;
+    public int Bitmask;
+
+    public Tile Left;
+    public Tile Right;
+    public Tile Top;
+    public Tile Bottom;
+
+    public bool Collidable;
+    public bool FloodFilled;
+
+    public Color Color = Color.black;
+
+    public Tile()
+    {
+    }
+
+    public void UpdatBitmask()
+    {
+        int count = 0;
+
+        if (Collidable && Top != null && Top.DensityType == DensityType)
+            count += 1;
+        if (Collidable && Bottom != null && Bottom.DensityType == DensityType)
+            count += 4;
+        if (Collidable && Left != null && Left.DensityType == DensityType)
+            count += 8;
+        if (Collidable && Right != null && Right.DensityType == DensityType)
+            count += 2;
+
+        Bitmask = count;
+    }
+
+}
